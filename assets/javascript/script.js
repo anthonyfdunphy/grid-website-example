bc9db23080a7326e2fa9a3aca99f22bc124c6546
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-07-16 19:16:15 +0000

    flexbox gallery with js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,70 +1,14 @@
-const inventors = [
-    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-];
+const panels = document.querySelectorAll('.panel')
 
-const people = [
-    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
-    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
-    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
-    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
-    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
-];
+function toggleOpen(){
+    this.classList.toggle('open');
+}
 
+function toggleActive(e){
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }
+}
 
-// 1. FIlter the list of inventors for those who were born in the 1500's
-const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year <= 1600));
-
-console.table(fifteen);
-
-// 2. Give us an array of the inventory first and last names
-
-const fullNames = inventors.map(inventor => `${inventor.first} + ${inventor.last}`)
-console.log(fullNames);
-
-// 3.Sort inventors by birthdate, oldest to youngest
-
-const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
-
-console.table(ordered)
-
-// 4. How many years did all the inventors live?
-
-const totalYears = inventors.reduce((total, inventor) => {
-    return total + (inventor.passed - inventor.year);
-}, 0);
-
-console.log(totalYears);
-
-// 5. Sort inventors by years lived
-
-const oldest = inventors.sort(function(a,b){
-    const lastGuy = a.passed - a.year;
-    const nextGuy = b.passed - b.year;
-    return lastGuy > nextGuy ? -1 : 1;
-});
-
-console.table(oldest);
-
-// 6.Create alist of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Parishttps://en.wikipedia.org/wiki/Category:Boulevards_in_Paris;
-
-const category = document.querySelector('.mw-category');
-const links = Array.from(category.querySelectorAll('a'));
-const de = links
-                .map(link => link.textContent)
-                .filter(streetName => streetName.includes('de'));
-
-// This is code to filter a website so that it shows only whats contained with DE in the name
-
-// pull information from the workmans website
-
-const workmansCat = document.querySelector('mw-category');
-const workLinks = workmansCat.querySelectorAll('list_entry');
-
-
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
